607ba98412788b2feba5da58c270c4361caab3be
johndear <ls123456>
2017-08-02 15:17:21 +0800

    1

--- src/main/java/com/op/Main.java
+++ src/main/java/com/op/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("--------- start ----------------");
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 200; i++) {
 			System.out.println(i);
 		}
 		Thread.sleep(60 * 1000);
