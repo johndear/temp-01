28b12fa87a52f79516bb4e3f3ae4870c8f4a0372
johndear <ls123456>
2017-08-02 14:31:59 +0800

    test

--- src/main/java/com/op/Main.java
+++ src/main/java/com/op/Main.java
@@ -2,8 +2,14 @@ package com.op;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		System.out.println("--------- main ----------------");
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("--------- start ----------------");
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+		Thread.sleep(60 * 1000);
+		System.out.println("--------- end! ----------------");
+		
 	}
 
 }
