7731f3993cddd028870af35fb034d8c0111de7cd
johndear <ls123456>
2017-08-24 10:44:08 +0800

    test

---
+++ src/main/java/com/op/service/a.java
@@ -0,0 +1,15 @@
+package com.op.service;
+
+public class a {
+	
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("--------- start ----------------");
+		for (int i = 0; i < 500; i++) {
+			System.out.println(i);
+		}
+		Thread.sleep(60 * 1000);
+		System.out.println("--------- end! ----------------");
+		
+	}
+
+}
